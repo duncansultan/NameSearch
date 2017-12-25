931a473528d603cd806ae99004ef7b72122322ba
Duncan Hill <duncansultan@gmail.com>
2017-12-25 16:27:49 -0600

    Add comments to Context Program class, indicating that it is intentionally left empty for Code First Migrations.

--- NameSearch.Context/Program.cs
+++ NameSearch.Context/Program.cs
@@ -1,7 +1,14 @@
 ﻿namespace NameSearch.Context
 {
+    /// <summary>
+    /// The Program class is required for executing code first database migrations.
+    /// See also project ReadMe.txt
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// This method is intentionally left blank.
+        /// </summary>
         public static void Main(string[] args)
         {
         }

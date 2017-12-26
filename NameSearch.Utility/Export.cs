da2b5bb08ac26c78d84556bc90e587e587ec8df7
Duncan Hill <duncansultan@gmail.com>
2017-12-26 10:34:45 -0600

    Add Import Export Csv Utilities.

--- NameSearch.Utility/Export.cs
+++ NameSearch.Utility/Export.cs
@@ -1,21 +1,42 @@
-﻿using CsvHelper.Configuration;
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using System.Collections;
+using System.IO;
 
 namespace NameSearch.Utility
 {
     public class Export
     {
         private readonly Configuration CsvHelperConfiguration;
-        private readonly string OutputDirectory;
+        private readonly string Directory;
 
-        public Export(Configuration csvHelperConfiguration, string outputDirectory)
+        public Export(string directory, Configuration csvHelperConfiguration)
         {
+            this.Directory = directory;
             this.CsvHelperConfiguration = csvHelperConfiguration;
-            this.OutputDirectory = outputDirectory;
         }
 
-        public void ToCsv<T>(T model, string fileName)
+        public Export(string directory)
         {
-            //ToDo: Use CSV Helper to Export
+            this.Directory = directory;
+            this.CsvHelperConfiguration = new Configuration() { QuoteAllFields = true };
+        }
+
+        public void ToCsv<T>(T records, string fileName, bool isAppend)
+            where T : IEnumerable
+        {
+            if (!fileName.EndsWith(".csv"))
+            {
+                fileName = $"{fileName}.csv";
+            }
+            var absolutePath = Path.Combine(Directory, fileName);
+
+
+            using (var textWriter = new StreamWriter(absolutePath, isAppend))
+            using (var csv = new CsvWriter(textWriter, CsvHelperConfiguration))
+            {
+                csv.WriteRecords(records);
+            }
         }
     }
 }
---
+++ NameSearch.Utility/Import.cs
@@ -0,0 +1,43 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using System.Collections.Generic;
+using System.IO;
+
+namespace NameSearch.Utility
+{
+    public class Import
+    {
+        private readonly Configuration CsvHelperConfiguration;
+        private readonly string Directory;
+
+        public Import(string directory, Configuration csvHelperConfiguration)
+        {
+            this.Directory = directory;
+            this.CsvHelperConfiguration = csvHelperConfiguration;
+        }
+
+        public Import(string directory)
+        {
+            this.Directory = directory;
+            this.CsvHelperConfiguration = new Configuration() { QuoteAllFields = true };
+        }
+
+        public IEnumerable<T> FromCsv<T>(string fileName, bool isAppend)
+        {
+            var absolutePath = Path.Combine(Directory, fileName);
+
+            var fileExists = File.Exists(absolutePath);
+            if (!fileExists)
+            {
+                throw new FileNotFoundException($"File {fileName} does not exist.");
+            }
+
+            using (var textReader = new StreamReader(absolutePath, isAppend))
+            using (var csv = new CsvReader(textReader, CsvHelperConfiguration))
+            {
+                var records = csv.GetRecords<T>();
+                return records;
+            }
+        }
+    }
+}

bbbd8e99060d966e99f5df4dacef726167dffc12
Duncan Hill <duncansultan@gmail.com>
2018-01-11 20:28:37 -0600

    Update Test Assertions.

--- NameSearch.Api.Tests/FindPersonController_ShouldReturnJsonResult.cs
+++ NameSearch.Api.Tests/FindPersonController_ShouldReturnJsonResult.cs
@@ -1,64 +1,54 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using NameSearch.Api.Controllers;
 using NameSearch.Api.Controllers.Interfaces;
 using NameSearch.Models.Domain;
+using Newtonsoft.Json;
 using System.Threading.Tasks;
 using Xunit;
 
 namespace NameSearch.Api.Tests
 {
-    
     public class FindPersonController_ShouldReturnJsonResult
     {
         private readonly IFindPersonController Controller;
 
         public FindPersonController_ShouldReturnJsonResult()
         {
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json");
+            var configuration = builder.Build();
+
+            var serializerSettings = new JsonSerializerSettings();
             //ToDo: Consider mocking this controller.
-            Controller = new FindPersonController();
+            Controller = new FindPersonController(configuration, serializerSettings);
         }
 
         [Fact]
         public async Task GetPerson()
         {
             //Arrange
-            IPerson model = new Person();
-            model.Address1= "";
-            model.Address2= "";
-            model.City= "";
-            model.Country= "";
-            model.Name= "";
-            model.State= "";
-            model.Zip= "";
+            IPerson model = new Person
+            {
+                Address1 = "",
+                Address2 = "",
+                City = "",
+                Country = "",
+                Name = "Mwangi",
+                State = "NC",
+                Zip = ""
+            };
 
             //Act
             var response = await Controller.GetPerson(model);
 
             //Assert
-            //Http OK
-            //Assert.Equal(HttpStatusCode.OK.ToString(), response.StatusCode.ToString());
-            //Assert.NotNull(response.Value);
-            //Assert.IsAssignableFrom<string>(response.Value);
-            //Assert.False(string.IsNullOrWhiteSpace(response.Value.ToString()), "Response string is empty null or whitespace.");
-            ////Deserialization Tests
-            //var deserializedObject = JsonConvert.DeserializeObject(response.Value.ToString());
-            //Assert.NotNull(deserializedObject);
-            //var deserializedPerson = JsonConvert.DeserializeObject<IPerson>(response.Value.ToString());
-            //Model Property Tests
-            //Assert.NotNull(deserializedPerson);
-            //Assert.NotNull(deserializedPerson.Address1);
-            //Assert.NotNull(deserializedPerson.Address2);
-            //Assert.NotNull(deserializedPerson.City);
-            //Assert.NotNull(deserializedPerson.Country);
-            //Assert.NotNull(deserializedPerson.Name);
-            //Assert.NotNull(deserializedPerson.State);
-            //Assert.NotNull(deserializedPerson.Zip);           
-            //Assert.Same(deserializedPerson.Address1, "");
-            //Assert.Same(deserializedPerson.Address2, "");
-            //Assert.Same(deserializedPerson.City, "");
-            //Assert.Same(deserializedPerson.Country, "");
-            //Assert.Same(deserializedPerson.Name, "");
-            //Assert.Same(deserializedPerson.State, "");
-            //Assert.Same(deserializedPerson.Zip, "");
+            Assert.NotNull(response);
+            Assert.IsAssignableFrom<JsonResult>(response);
+            Assert.NotNull(response.SerializerSettings);
+            Assert.NotNull(response.Value);
+            Assert.IsAssignableFrom<string>(response.Value);
+            Assert.False(string.IsNullOrWhiteSpace(response.Value.ToString()), "Response string is empty null or whitespace.");
         }
     }
 }

61ff3042696ac36e6d2d625761c71c400c14fa36
Duncan Hill <duncansultan@gmail.com>
2018-01-18 21:37:19 -0600

    Modify FindPersonController_ShouldReturnJsonResult xunit test to Disabled.

--- NameSearch.Api.Tests/FindPersonController_ShouldReturnJsonResult.cs
+++ NameSearch.Api.Tests/FindPersonController_ShouldReturnJsonResult.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using NameSearch.Api.Controllers;
 using NameSearch.Api.Controllers.Interfaces;
-using NameSearch.Models.Domain;
+using NameSearch.Models.Domain.Api.Request;
 using Newtonsoft.Json;
 using System.Threading.Tasks;
 using Xunit;
@@ -20,24 +20,16 @@ namespace NameSearch.Api.Tests
             var configuration = builder.Build();
 
             var serializerSettings = new JsonSerializerSettings();
-            //ToDo: Consider mocking this controller.
-            Controller = new FindPersonController(configuration, serializerSettings);
+            this.Controller = new FindPersonController(configuration, serializerSettings);
         }
 
-        [Fact]
+#pragma warning disable xUnit1004 // Test methods should not be skipped
+        [Fact(Skip = "Integration test incurs financial expense and should be manually initiated.")]
+#pragma warning restore xUnit1004 // Test methods should not be skipped
         public async Task GetPerson()
         {
             //Arrange
-            IPerson model = new PersonSearch
-            {
-                Address1 = "",
-                Address2 = "",
-                City = "",
-                Country = "",
-                Name = "Mwangi",
-                State = "NC",
-                Zip = ""
-            };
+            var model = GetTestPerson();
 
             //Act
             var response = await Controller.GetPerson(model);
@@ -49,6 +41,21 @@ namespace NameSearch.Api.Tests
             Assert.NotNull(response.Value);
             Assert.IsAssignableFrom<string>(response.Value);
             Assert.False(string.IsNullOrWhiteSpace(response.Value.ToString()), "Response string is empty null or whitespace.");
+        }
+
+        private Person GetTestPerson()
+        {
+            var person = new Person
+            {
+                Address1 = "",
+                Address2 = "",
+                City = "",
+                Country = "",
+                Name = "Mwangi",
+                State = "NC",
+                Zip = ""
+            };
+            return person;
         }
     }
 }

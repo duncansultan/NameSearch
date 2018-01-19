985eee4d9e996f20fcde92e9b9eda7f46434241a
Duncan Hill <duncansultan@gmail.com>
2018-01-18 21:32:08 -0600

    Add entity SearchNameGroup.

---
+++ NameSearch.Models/Entities/SearchNameGroup.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using NameSearch.Models.Entities.Abstracts;
+
+namespace NameSearch.Models.Entities
+{
+    /// <summary>
+    /// Search Name Group Entity
+    /// </summary>
+    /// <seealso cref="NameSearch.Models.Entities.Abstracts.EntityBase{NameSearch.Models.Entities.SearchNameGroup}" />
+    /// <inheritdoc />
+    public class SearchNameGroup : EntityBase<SearchNameGroup>
+    {
+        /// <summary>
+        /// Gets or sets the name of the file.
+        /// </summary>
+        /// <value>
+        /// The name of the file.
+        /// </value>
+        public string FileName { get; set; }
+        /// <summary>
+        /// Gets or sets the searches.
+        /// </summary>
+        /// <value>
+        /// The searches.
+        /// </value>
+        public List<SearchName> SearchNames { get; set; }
+
+        #region Equality
+
+        /// <summary>
+        /// See http://www.aaronstannard.com/overriding-equality-in-dotnet/
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public override bool Equals(SearchNameGroup other)
+        {
+            if (other == null) return false;
+            return FileName == other.FileName &&
+                SearchNames.Equals(other.SearchNames);
+        }
+
+        /// <summary>
+        /// Determines whether the specified <see cref="System.Object" />, is equal to this instance.
+        /// </summary>
+        /// <param name="obj">The <see cref="System.Object" /> to compare with this instance.</param>
+        /// <returns>
+        ///   <c>true</c> if the specified <see cref="System.Object" /> is equal to this instance; otherwise, <c>false</c>.
+        /// </returns>
+        public override bool Equals(object obj)
+        {
+            if (obj is null) return false;
+            if (this is null) return false;
+            if (obj.GetType() != GetType()) return false;
+            return Equals(obj as SearchJob);
+        }
+
+        /// <summary>
+        /// Return Base Implementation.
+        /// "You should only override GetHashCode if your objects are immutable."
+        /// See also http://www.aaronstannard.com/overriding-equality-in-dotnet/
+        /// See also https://stackoverflow.com/questions/263400/what-is-the-best-algorithm-for-an-overridden-system-object-gethashcode/263416#263416
+        /// </summary>
+        /// <returns>
+        /// A hash code for this instance, suitable for use in hashing algorithms and data structures like a hash table. 
+        /// </returns>
+        public override int GetHashCode() => base.GetHashCode();
+
+        #endregion
+    }
+}

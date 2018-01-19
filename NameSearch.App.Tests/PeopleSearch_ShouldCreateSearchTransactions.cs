a7072b286a0f2fa2a2446a45a695a8b736e652a0
Duncan Hill <duncansultan@gmail.com>
2018-01-19 01:59:51 -0600

    Finish PeopleSearch_ShouldCreateSearchTransactions unit test.

--- NameSearch.App.Tests/PeopleSearch_ShouldCreateSearchTransactions.cs
+++ NameSearch.App.Tests/PeopleSearch_ShouldCreateSearchTransactions.cs
@@ -5,8 +5,11 @@ using NameSearch.Api.Controllers.Interfaces;
 using NameSearch.App.Tasks;
 using NameSearch.Repository;
 using NameSearch.Utility.Interfaces;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -24,9 +27,28 @@ namespace NameSearch.Api.Tests
 
         public PeopleSearch_ShouldCreateSearchTransactions()
         {
-            this.MockRepository = new Mock<IEntityFrameworkRepository>();
-            this.MockFindPersonController = new Mock<IFindPersonController>();
-            this.MockExport = new Mock<IExport>();
+            MockRepository = new Mock<IEntityFrameworkRepository>();
+            //Config Mock
+            MockRepository.Setup(x => x.GetAll<Models.Entities.SearchName>(null, null, null, null)).Returns(GetTestSearchNames());
+            MockRepository.Setup(x => x.GetAllAsync<Models.Entities.SearchName>(null, null, null, null)).Returns(Task.FromResult(GetTestSearchNames()));
+            MockRepository.Setup(x => x.Create(It.IsAny<Models.Entities.SearchJob>()));
+            MockRepository.Setup(x => x.Create(It.IsAny<Models.Entities.SearchTransaction>()));
+            MockRepository.Setup(x => x.Update(It.IsAny<Models.Entities.SearchJob>()));
+            MockRepository.Setup(x => x.Update(It.IsAny<Models.Entities.SearchTransaction>()));
+            MockRepository.Setup(x => x.Save());
+            MockRepository.Setup(x => x.SaveAsync()).Returns(Task.CompletedTask);
+     
+            MockFindPersonController = new Mock<IFindPersonController>();
+            //Config Mock
+            MockFindPersonController.Setup(x => x.GetPerson(It.IsAny<Models.Domain.Api.Request.Person>())).Returns((Models.Domain.Api.Request.Person p) => Task.FromResult(GetJsonResult(p.Name, p.City)));
+
+            MockExport = new Mock<IExport>();
+            //Config Mock
+            MockExport.Setup(x => x.ToJson(It.IsAny<string>(), It.IsAny<string>()));
+            MockExport.Setup(x => x.ToJson(It.IsAny<JObject>(), It.IsAny<string>()));
+            MockExport.Setup(x => x.ToJsonAsync(It.IsAny<string>(), It.IsAny<string>())).Returns(Task.CompletedTask);
+            MockExport.Setup(x => x.ToJsonAsync(It.IsAny<JObject>(), It.IsAny<string>(), It.IsAny<CancellationToken>())).Returns(Task.CompletedTask);
+
             this.PeopleSearch = new PeopleSearch(MockRepository.Object, MockFindPersonController.Object, MockExport.Object);
         }
 
@@ -34,34 +56,79 @@ namespace NameSearch.Api.Tests
         public async Task LoadPeople()
         {
             // Arrange
-            //ToDo: Setup Mocks
-            //this.MockExport.Setup(x => x.ToJson())
             var people = GetTestPeople();
 
             // Act
-            IProgress<Models.Domain.Api.Request.Person> progress = new Progress<Models.Domain.Api.Request.Person>();
-            var result = await PeopleSearch.Run(people, progress);
+            var progress = new Progress<Models.Domain.Api.Request.Person>();
+            var cancellationToken = new CancellationToken();
+            var result = await PeopleSearch.Run(people, progress, cancellationToken);
 
             // Assert
-            var viewResult = Assert.IsType<JsonResult>(result);
-            //var model = Assert.IsAssignableFrom<IEnumerable<StormSessionViewModel>>(
-            //    viewResult.ViewData.Model);
-            //Assert.Equal(2, model.Count());
+            Assert.IsType<bool>(result);
+            Assert.True(result);
+
+            MockRepository.Verify(c => c.Create(It.IsAny<Models.Entities.SearchJob>()), Times.Once);
+            MockRepository.Verify(c => c.Create(It.IsAny<Models.Entities.SearchTransaction>()), Times.Exactly(people.Count()));
+            MockRepository.Verify(c => c.SaveAsync(), Times.Exactly(people.Count() + 1));
+            MockFindPersonController.Verify(c => c.GetPerson(It.IsAny<Models.Domain.Api.Request.Person>()), Times.Exactly(people.Count()));
+            MockExport.Verify(c => c.ToJsonAsync(It.IsAny<JObject>(), It.IsAny<string>(), It.IsAny<CancellationToken>()), Times.Exactly(people.Count()));
         }
 
-        private List<Models.Domain.Api.Request.Person> GetTestPeople()
+        private JsonResult GetJsonResult(string name, string city)
         {
-            var people = new List<Models.Domain.Api.Request.Person>();
-            people.Add(new Models.Domain.Api.Request.Person
+            var person = new JObject
             {
-                Address1 = "",
-                Address2 = "",
-                City = "",
-                Country = "",
-                Name = "Mwangi",
-                State = "NC",
-                Zip = ""
-            });
+                new JProperty("name", name),
+                new JProperty("city", city)
+            };
+
+            var jObject = new JObject
+            {
+                new JProperty("count_person", "1"),
+                new JProperty("warnings", ""),
+                new JProperty("error", ""),
+                new JProperty("person", person)
+            };
+            var result = new JsonResult(jObject.ToString())
+            {
+                StatusCode = (int)System.Net.HttpStatusCode.OK
+            };
+            return result;
+        }
+
+        private IEnumerable<Models.Domain.Api.Request.Person> GetTestPeople()
+        {
+            var people = new List<Models.Domain.Api.Request.Person>
+            {
+                new Models.Domain.Api.Request.Person
+                {
+                    Address1 = "",
+                    Address2 = "",
+                    City = "",
+                    Country = "",
+                    Name = "Mwangi",
+                    State = "NC",
+                    Zip = ""
+                }
+            };
+            return people;
+        }
+
+        private IEnumerable<Models.Entities.SearchName> GetTestSearchNames()
+        {
+            var people = new List<Models.Entities.SearchName>
+            {
+                new Models.Entities.SearchName
+                {
+                    Id = 1,
+                    Value = "Mwangi",
+                    Description = "Kenya",
+                    IsActive = true,
+                    ModifiedDateTime = DateTime.Now,
+                    SearchNameGroupId = 1,
+                    CreatedDateTime = DateTime.Now
+                }
+            };
             return people;
         }
     }

55d3345d43c53e42bc43ea820e5ccc9dd4ad560f
Duncan Hill <duncansultan@gmail.com>
2018-01-19 23:14:50 -0600

    Implement global exception handling.

--- NameSearch.App/Program.cs
+++ NameSearch.App/Program.cs
@@ -22,39 +22,80 @@ namespace NameSearch.App
                 .WriteTo.Console()
                 .CreateLogger();
 
-            #region Dependency Injection Container
-
-            // use Dependency injection in console app https://andrewlock.net/using-dependency-injection-in-a-net-core-console-application/
-            // add the framework services
-            var services = new ServiceCollection()
-                .AddLogging(loggingBuilder => loggingBuilder.AddSerilog(dispose: true))
-                //.AddSingleton<DbContext, ApplicationDbContext>()
-                .AddDbContext<ApplicationDbContext>(options => options.UseSqlite("Data Source=blog.db"))
-                .AddSingleton<IEntityFrameworkRepository, EntityFrameworkRepository>();
-
-            services.AddMvc();
-
-            // add StructureMap
-            var container = new Container();
-            container.Configure(config =>
+            try
             {
-                // Register stuff in container, using the StructureMap APIs...
-                config.Scan(_ =>
+                //ToDo: Parse args for parameters                
+
+                #region Dependency Injection Container
+
+                // use Dependency injection in console app https://andrewlock.net/using-dependency-injection-in-a-net-core-console-application/
+                // add the framework services
+                var services = new ServiceCollection()
+                    .AddLogging(loggingBuilder => loggingBuilder.AddSerilog(dispose: true))
+                    //.AddSingleton<DbContext, ApplicationDbContext>()
+                    .AddDbContext<ApplicationDbContext>(options => options.UseSqlite("Data Source=blog.db"))
+                    .AddSingleton<IEntityFrameworkRepository, EntityFrameworkRepository>();
+
+                services.AddMvc();
+
+                // add StructureMap
+                var container = new Container();
+                container.Configure(config =>
                 {
-                    _.AssemblyContainingType(typeof(Program));
-                    _.WithDefaultConventions();
+                    // Register stuff in container, using the StructureMap APIs...
+                    config.Scan(_ =>
+                    {
+                        _.AssemblyContainingType(typeof(Program));
+                        _.WithDefaultConventions();
+                    });
+                    // Populate the container using the service collection
+                    config.Populate(services);
                 });
-                // Populate the container using the service collection
-                config.Populate(services);
-            });
 
-            #endregion
+                #endregion
 
-            var serviceProvider = container.GetInstance<IServiceProvider>();
-            var dbContext = serviceProvider.GetService<DbContext>();
-            var repository = serviceProvider.GetService<IEntityFrameworkRepository>();
+                var serviceProvider = container.GetInstance<IServiceProvider>();
+                var dbContext = serviceProvider.GetService<DbContext>();
+                var repository = serviceProvider.GetService<IEntityFrameworkRepository>();
 
-            Console.WriteLine("Hello World!");
+                //ToDo: Test Directory exists and has access
+
+                //ToDo: Create User Interface with command line
+                Console.WriteLine("Hello World!");
+
+                //ToDo: Option to Import Names using SearchNameImporter
+
+                //ToDo: Parse city, state, zip from console input
+                string city;
+                string state;
+                string zip;
+
+                //ToDo: Get List of searchNames
+                //var nameImport = Repository.GetFirst<NameImport>(null, o => o.OrderByDescending(y => y.Id));
+                //var names = Repository.Get<Name>(x => x.NameImportId == nameImport.Id, o => o.OrderByDescending(y => y.Value));
+                //var people = names.Select(x => new NameSearch.Models.Domain.Api.Request.Person
+                //{
+                //    Name = x.Value,
+                //    City = city,
+                //    State = state,
+                //    Zip = zip
+                //}).ToList();
+
+                //ToDo: Execute PeopleFinder.Run() in batches of x (from config file) records.
+
+                //ToDo: Execute PeopleFinder.Run()
+                //ToDo: Execute PeopleSearchResultProcessor.Run()
+                //ToDo: Execute Export.ToCsv();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Fatal Application Failure");
+                throw;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
     }
 }

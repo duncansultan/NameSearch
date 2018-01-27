34cd54db54a20e7020dc36abb269cbb1cae77eec
Duncan Hill <duncansultan@gmail.com>
2018-01-27 00:29:18 -0600

    Create IPeopleFinder interace

---
+++ NameSearch.App/Tasks/Interfaces/IPeopleFinder.cs
@@ -0,0 +1,24 @@
+﻿using NameSearch.Repository;
+using System.Threading.Tasks;
+using NameSearch.Api.Controllers.Interfaces;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System;
+using System.Threading;
+using NameSearch.Models.Utility;
+
+namespace NameSearch.App.Tasks
+{
+    /// <summary>
+    /// Run Searches to Find People
+    /// </summary>
+    public interface IPeopleFinder
+    {
+        /// <summary>
+        /// Runs the specified people.
+        /// </summary>
+        /// <param name="people">The people.</param>
+        /// <returns></returns>
+        Task<bool> Run<TProgressReport>(IEnumerable<Models.Domain.Api.Request.IPerson> people, IProgress<TProgressReport> progress, CancellationToken cancellationToken) where TProgressReport : IProgressReport, new();
+    }
+}

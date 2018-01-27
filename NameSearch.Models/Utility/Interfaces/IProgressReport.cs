1cf8606a1564128f58b48772af1b42061ded633c
Duncan Hill <duncansultan@gmail.com>
2018-01-27 00:28:29 -0600

    Add ProgressReport model

---
+++ NameSearch.Models/Utility/Interfaces/IProgressReport.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NameSearch.Models.Utility
+{
+    public interface IProgressReport
+    {
+        bool IsRunning { get; set; }
+        TimeSpan ElapsedTimeSpan { get; set; }
+        int ProgressCount { get; set; }
+        int TotalCount { get; set; }
+        double PercentRemaining { get; set; }
+        string Message { get; set; }
+        void IncrementCount();
+        void UpdatePercentRemaining();
+        void UpdateRemaining();
+    }
+}
---
+++ NameSearch.Models/Utility/ProgressReport.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace NameSearch.Models.Utility
+{
+    public class ProgressReport : IProgressReport
+    {
+        public bool IsRunning { get; set; }
+        public TimeSpan ElapsedTimeSpan { get; set; }
+        public int ProgressCount { get; set; }
+        public int TotalCount { get; set; } = 1;
+        public double PercentRemaining { get; set; } = 0.00;
+        public string Message { get; set; }
+        public void IncrementCount()
+        {
+            ProgressCount++;
+        }
+        public void UpdatePercentRemaining()
+        {
+            if (TotalCount < 1)
+            {
+                TotalCount = 1;
+            }
+            PercentRemaining = ProgressCount / TotalCount;
+        }
+        public void UpdateRemaining()
+        {
+            ProgressCount++;
+            PercentRemaining = ProgressCount / TotalCount;
+        }
+    }
+}

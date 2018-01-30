3f37650b4e3b139b10ed8e94c59f4bc41c54b9e8
Duncan Hill <duncansultan@gmail.com>
2018-01-29 23:31:45 -0600

    Add SearchCriteria domain model

---
+++ NameSearch.Models/Domain/SearchCriteria.cs
@@ -0,0 +1,48 @@
+﻿namespace NameSearch.Models.Domain
+{
+    public class SearchCriteria
+    {
+        /// <summary>
+        /// Gets or sets the address1.
+        /// </summary>
+        /// <value>
+        /// The address1.
+        /// </value>
+        public string Address1 { get; set; }
+        /// <summary>
+        /// Gets or sets the address2.
+        /// </summary>
+        /// <value>
+        /// The address2.
+        /// </value>
+        public string Address2 { get; set; }
+        /// <summary>
+        /// Gets or sets the city.
+        /// </summary>
+        /// <value>
+        /// The city.
+        /// </value>
+        public string City { get; set; }
+        /// <summary>
+        /// Gets or sets the state.
+        /// </summary>
+        /// <value>
+        /// The state.
+        /// </value>
+        public string State { get; set; }
+        /// <summary>
+        /// Gets or sets the zip.
+        /// </summary>
+        /// <value>
+        /// The zip.
+        /// </value>
+        public string Zip { get; set; }
+        /// <summary>
+        /// Gets or sets the country.
+        /// </summary>
+        /// <value>
+        /// The country.
+        /// </value>
+        public string Country { get; set; }
+    }
+}

4a7e4303530da28a3249b821945c2557ce30d190
Duncan Hill <duncansultan@gmail.com>
2018-01-31 07:20:52 -0600

    Add People Factory

---
+++ NameSearch.App/Factories/PeopleFactory.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+
+namespace NameSearch.App.Factories
+{
+    /// <summary>
+    /// Factory to Create Person Models from Person Entity with multiple Address records
+    /// </summary>
+    public static class PeopleFactory
+    {
+        /// <summary>
+        /// Gets the specified person entity.
+        /// </summary>
+        /// <param name="personEntity">The person entity.</param>
+        /// <returns></returns>
+        public static IEnumerable<Models.Domain.Person> Get(Models.Entities.Person personEntity)
+        {
+            var people = new List<Models.Domain.Person>();
+
+            foreach (var address in personEntity.Addresses)
+            {
+                var person = Initialize(personEntity);
+                person.Address1 = address.Address1;
+                person.Address2 = address.Address2;
+                person.City = address.City;
+                person.State = address.State;
+                person.Zip = address.Zip;
+                person.Plus4 = address.Plus4;
+                person.Country = address.Country;
+                person.Lattitude = address.Latitude;
+                person.Longitude = address.Longitude;
+                people.Add(person);
+            }
+
+            return people;
+        }
+
+        /// <summary>
+        /// Initializes the specified person entity.
+        /// </summary>
+        /// <param name="personEntity">The person entity.</param>
+        /// <returns></returns>
+        private static Models.Domain.Person Initialize(Models.Entities.Person personEntity)
+        {
+            return new Models.Domain.Person
+            {
+                FirstName = personEntity.FirstName,
+                LastName = personEntity.LastName,
+                AgeRange = personEntity.AgeRange
+            };
+        }
+    }
+}

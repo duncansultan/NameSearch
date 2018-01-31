6f0cb26f291f4eca896983c08d50d88d2cc90730
Duncan Hill <duncansultan@gmail.com>
2018-01-31 07:20:37 -0600

    Add Mock Factory for Import Utility

---
+++ NameSearch.App.Tests/Mocks/MockImportFactory.cs
@@ -0,0 +1,25 @@
+﻿using Moq;
+using NameSearch.Utility.Interfaces;
+using System.Threading;
+
+namespace NameSearch.App.Tests.Mocks
+{
+    /// <summary>
+    /// Mock for Import Utility
+    /// </summary>
+    public static class MockImportFactory
+    {
+        /// <summary>
+        /// Gets this instance.
+        /// </summary>
+        /// <returns></returns>
+        public static Mock<IImport> Get()
+        {
+            var MockImport = new Mock<IImport>();
+            MockImport.Setup(x => x.FromCsv<string>(It.IsAny<string>())).Returns(MockDataFactory.GetTestNames());
+            MockImport.Setup(x => x.FromJson(It.IsAny<string>()));
+            MockImport.Setup(x => x.FromJsonAsync(It.IsAny<string>(), It.IsAny<CancellationToken>()));
+            return MockImport;
+        }
+    }
+}

6253221be8c7f69cf926691212557e36f891c029
Duncan Hill <duncansultan@gmail.com>
2018-01-31 07:17:33 -0600

    Add Person domain class used in export

--- NameSearch.Models/Domain/Person.cs
+++ NameSearch.Models/Domain/Person.cs
@@ -5,5 +5,96 @@
     /// </summary>
     public class Person
     {
+        /// <summary>
+        /// Gets or sets the first name.
+        /// </summary>
+        /// <value>
+        /// The first name.
+        /// </value>
+        public string FirstName { get; set; }
+        /// <summary>
+        /// Gets or sets the last name.
+        /// </summary>
+        /// <value>
+        /// The last name.
+        /// </value>
+        public string LastName { get; set; }
+        /// <summary>
+        /// Gets or sets the age range.
+        /// </summary>
+        /// <value>
+        /// The age range.
+        /// </value>
+        public string AgeRange { get; set; }
+        /// <summary>
+        /// Gets or sets the phone number.
+        /// </summary>
+        /// <value>
+        /// The phone number.
+        /// </value>
+        public string PhoneNumber { get; set; }
+        /// <summary>
+        /// Gets or sets the address1.
+        /// </summary>
+        /// <value>
+        /// The address1.
+        /// </value>
+        public string Address1 { get; set; }
+        /// <summary>
+        /// Gets or sets the address2.
+        /// </summary>
+        /// <value>
+        /// The address2.
+        /// </value>
+        public string Address2 { get; set; }
+        /// <summary>
+        /// Gets or sets the city.
+        /// </summary>
+        /// <value>
+        /// The city.
+        /// </value>
+        public string City { get; set; }
+        /// <summary>
+        /// Gets or sets the state.
+        /// </summary>
+        /// <value>
+        /// The state.
+        /// </value>
+        public string State { get; set; }
+        /// <summary>
+        /// Gets or sets the zip.
+        /// </summary>
+        /// <value>
+        /// The zip.
+        /// </value>
+        public string Zip { get; set; }
+        /// <summary>
+        /// Gets or sets the plus4.
+        /// </summary>
+        /// <value>
+        /// The plus4.
+        /// </value>
+        public string Plus4 { get; set; }
+        /// <summary>
+        /// Gets or sets the country.
+        /// </summary>
+        /// <value>
+        /// The country.
+        /// </value>
+        public string Country { get; set; }
+        /// <summary>
+        /// Gets or sets the lattitude.
+        /// </summary>
+        /// <value>
+        /// The lattitude.
+        /// </value>
+        public double? Lattitude { get; set; }
+        /// <summary>
+        /// Gets or sets the longitude.
+        /// </summary>
+        /// <value>
+        /// The longitude.
+        /// </value>
+        public double? Longitude { get; set; }
     }
 }

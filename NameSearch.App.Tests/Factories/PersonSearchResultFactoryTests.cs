56f72293f6268cb6763d6767f10ae325c52cc924
Duncan Hill <duncansultan@gmail.com>
2018-01-30 22:44:11 -0600

    Create unit test for PersonSearchResultFactory

---
+++ NameSearch.App.Tests/Factories/PersonSearchResultFactoryTests.cs
@@ -0,0 +1,32 @@
+﻿using NameSearch.App.Factories;
+using NameSearch.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using Xunit;
+
+namespace NameSearch.App.Tests
+{
+    public class PersonSearchResultFactoryTests
+    {
+        [Fact]
+        public void Create_ValidInput_ReturnSearchResult()
+        {
+            // Arrange
+            var personSearchRequestId = MockDataFactory.GetPersonSearchRequest().Id;
+            var httpStatusCode = (int)HttpStatusCode.OK;
+            var jObject = MockDataFactory.GetExampleJObject();
+          
+            // Act
+            var result = PersonSearchResultFactory.Create(personSearchRequestId, httpStatusCode, jObject);
+
+            // Assert
+            Assert.IsType<PersonSearchResult>(result);
+            Assert.NotNull(result);
+            Assert.Equal(personSearchRequestId, result.PersonSearchRequestId);
+            Assert.Equal(httpStatusCode, result.HttpStatusCode);
+            Assert.Equal(jObject.ToString(), result.Data);
+        }
+    }
+}

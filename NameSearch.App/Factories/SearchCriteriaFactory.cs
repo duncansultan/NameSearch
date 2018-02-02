b1229ea406d63a3d5be9245622d82551cff95890
Duncan Hill <duncansultan@gmail.com>
2018-02-02 00:40:41 -0600

    Create SearchCriteriaFactory

---
+++ NameSearch.App/Factories/SearchCriteriaFactory.cs
@@ -0,0 +1,30 @@
+﻿using NameSearch.Models.Domain;
+
+namespace NameSearch.App.Factories
+{
+    /// <summary>
+    /// Factory for SearchCritera model
+    /// </summary>
+    public static class SearchCriteriaFactory
+    {
+        /// <summary>
+        /// Gets the specified city.
+        /// </summary>
+        /// <param name="city">The city.</param>
+        /// <param name="state">The state.</param>
+        /// <param name="zip">The zip.</param>
+        /// <returns></returns>
+        public static SearchCriteria Get(string city, string state, string zip)
+        {
+            return new SearchCriteria
+            {
+                Address1 = string.Empty,
+                Address2 = string.Empty,
+                City = city,
+                State = state,
+                Zip = zip,
+                Country = "US"
+            };
+        }
+    }
+}

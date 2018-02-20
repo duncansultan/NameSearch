3fdf17370822d6e02068d443395b673cdc5106ee
Duncan Hill <duncansultan@gmail.com>
2018-02-19 23:09:35 -0600

    Create SearchResponseJObjectFactory class

---
+++ NameSearch.App/Factories/SearchResponseJObjectFactory.cs
@@ -0,0 +1,27 @@
+﻿using NameSearch.Models.Domain;
+using Newtonsoft.Json.Linq;
+
+namespace NameSearch.App.Factories
+{
+    /// <summary>
+    /// Search Response JObject Factory
+    /// </summary>
+    public static class SearchResponseJObjectFactory
+    {
+        /// <summary>
+        /// Gets the specified search.
+        /// </summary>
+        /// <param name="search">The search.</param>
+        /// <param name="jResult">The j result.</param>
+        /// <returns></returns>
+        public static JObject Get(Search search, JObject jResult)
+        {
+            var jSearch = JObject.FromObject(search);
+            return new JObject
+            {
+                { "search", jSearch },
+                { "result", jResult }
+            };
+        }
+    }
+}
